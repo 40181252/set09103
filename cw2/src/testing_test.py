341639ac97f343b6e9c4a9c6f88d161a287615e8
Tibor Gazsovics <40181252@live.napier.ac.uk>
2015-11-29 21:59:11 +0000

    testing coursework

---
+++ cw2/src/testing_test.py
@@ -0,0 +1,15 @@
+import os
+import unittest
+import tempfile
+import coursework2
+
+class TestingTest(unittest.TestCase):
+  def test_root(self):
+    self.app = coursework2.app.test_client()
+    out = self.app.get('/')
+    assert '200 OK' in out.status
+    assert 'charset=utf-8' in out.content_type
+    assert 'text/html' in out.content_type
+
+if __name__ == "__main__":
+  unittest.main()
